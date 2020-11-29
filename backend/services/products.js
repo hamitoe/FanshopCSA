43487cd147e0f1ba3c2232d263a4547c45b97532
CarinaSchulz <schulzc3@hs-albsig.de>
2020-11-29 15:49:02 +0100

    Revert "Merge branch 'main' of https://github.com/hamitoe/FanshopCSA into main"
    This reverts commit 42962e11017a853487767c7069fc917e973bd96e, reversing
    changes made to 1d67657bd84b582027ef3f1ec08737718f789412.

--- backend/services/products.js
+++ backend/services/products.js
@@ -1,55 +1,235 @@
-const helper = require("../helper.js");
 const express = require("express");
 var serviceRouter = express.Router()
-const usersDao = require("../dao/usersDao.js");
+const productsDao = require("../dao/productsDao.js");
+
+const jwt = require("jsonwebtoken");
+const { response } = require("express");
+const jwtKey = "my_secret_key"
 
 
-serviceRouter.get("/sortiment", function (req, res) {
 
-  var sortiment = new usersDao(req.app.locals.dbConnection)
+serviceRouter.get("/speisekarte/alle", function (req, res) {
+
+  var productsDa = new productsDao(req.app.locals.dbConnection)
   try {
-    var products = sortiment.getSortiment();
+    var products = productsDa.getAllProducts();
     res.send(products).status(200)
   } catch (error) {
     return res.status(400).end()
   }
-});
-
-serviceRouter.post("/insert", function (req, res) {
-  helper.log("Service Land: Client requested creation of new record");
-
-  var errorMsgs = [];
-  if (helper.isUndefined(req.body.Vorname))
-    errorMsgs.push("Vorname fehlt");
-  if (helper.isUndefined(req.body.Nachname))
-    errorMsgs.push("Nachname fehlt");
-  if (helper.isUndefined(req.body.AdressID))
-    errorMsgs.push("AdressID fehlt");
-  if (helper.isUndefined(req.body.Email))
-    errorMsgs.push("Email fehlt");
-  if (helper.isUndefined(req.body.Passwort))
-    errorMsgs.push("Passwort fehlt");
+})
 
 
-  if (errorMsgs.length > 0) {
-    helper.log("Service Land: Creation not possible, data missing");
-    res.status(400).json(helper.jsonMsgError("Hinzufügen nicht möglich. Fehlende Daten: " + helper.concatArray(errorMsgs)));
-    return;
-  }
+serviceRouter.get("/speisekarte/speiseart/:speiseartId", function (req, res) {
 
-
-  const createUser = new usersDao(req.app.locals.dbConnection);
+  var productsDa = new productsDao(req.app.locals.dbConnection)
   try {
-    var result = createUser.create(req.body.Vorname, req.body.Nachname, req.body.AdressID, req.body.Email, req.body.Passwort);
-    helper.log("Service Land: Record inserted");
-    res.status(200).json(helper.jsonMsgOK(result));
-  } catch (ex) {
-    helper.logError("Service Land: Error creating new record. Exception occured: " + ex.message);
-    res.status(400).json(helper.jsonMsgError(ex.message));
+    var products = productsDa.getSpeiseBySpeiseart(req.params.speiseartId);
+    res.send(products)
+  } catch (error) {
+    return res.status(400).end()
   }
+
+})
+
+
+serviceRouter.get("/speisekarte/details", function (req, res) {
+
+  var productsDa = new productsDao(req.app.locals.dbConnection)
+  var param = req.query.Id
+  try {
+    var details = productsDa.getDetails(param)
+    res.send(details)
+  } catch (error) {
+    return res.status(400).end()
+  }
+
+})
+
+serviceRouter.post("/speisekarte/addItem", function (req, res) {
+  var header = req.headers.token
+  if (header == "null") {
+    res.status(401).end()
+  }
+  jwt.verify(header, jwtKey, function (err, decoded) {
+    if (err) {
+      res.status(401).end(); 
+    }
+    else {
+      var productsDa = new productsDao(req.app.locals.dbConnection);
+      var GerichtID = req.body.produkt.GerichtID;
+      var UserID = req.body.produkt.UserID;
+      var menge =  productsDa.exist(GerichtID, UserID);
+      if (menge != undefined) {
+        menge.Menge = menge.Menge + 1
+        productsDa.updateMenge(GerichtID, UserID, menge)
+       
+      res.status(200).send("hat geklappt");
+      }
+      else{
+      var response = productsDa.addToSpeisekorb(GerichtID, UserID)
+      res.status(200).send("hat geklappt")
+      }
+    }
+  })
+})
+
+serviceRouter.get("/speisekorb", function (req, res) {
+
+  var header = req.headers.token
+  if (header == "null") {
+    res.status(401).end()
+  }
+  jwt.verify(header, jwtKey, function (err, decoded) {
+    if (err) {
+      res.status(401).end();
+    }
+    else {
+      var gerichteSql = []
+      var productsDa = new productsDao(req.app.locals.dbConnection);
+      var products = productsDa.getSpeisekorb(req.query.Id)
+      products.forEach(element => {
+        gerichteSql.push(productsDa.getGericht(element.GerichtID))
+      });
+      res.status(200).send(gerichteSql)
+    }
+  })
+})
+
+serviceRouter.delete("/speisekorb/deleteItem", function (req, res) {
+  var header = req.headers.token
+  if (header == "null") {
+    res.status(401).end()
+  }
+  jwt.verify(header, jwtKey, function (err, decoded) {
+    if (err) {
+      res.status(401).end();
+    }
+    else {
+      var productsDa = new productsDao(req.app.locals.dbConnection);
+      var response = productsDa.deleteItem(req.body.id)
+      return res.status(200).end()
+    }
+  })
+})
+
+serviceRouter.get("/bestellung/alleLieferzeiten", function (req, res) {
+
+  var a = [];
+  var tId =1;
+  var stunde = 13;
+  var minute = 0;
+  var tmp = "";
+  var date = new Date();
+  var hours = date.getHours();
+  var mins = date.getMinutes();
+  var time = "" + hours + ":" + mins;
+  console.log("Uhrzeit:",time);
+  do{
+      tmp = "" + stunde + ":";
+      if (minute < 10 )
+      tmp += "0";
+      tmp += "" + minute;
+      var obj = {
+        'TerminID': tId++,
+        'Uhrzeit': tmp, 
+        'Frei': true
+      };
+      if (obj.Uhrzeit < time ) {
+        obj.Frei = false;
+      }
+      a.push(obj);
+      if (minute == 45 ){
+        stunde++;
+        minute=0;
+      }
+      else{
+        minute += 15;
+      }
+     
+      if (stunde == 19 && minute == 15)
+      break;
+  } while ( true);
+  var productsDa = new productsDao(req.app.locals.dbConnection);
+      var uhrzeitID = productsDa.uhrzeitFrei();
+    for (let index = 0; index < uhrzeitID.length; index++) {
+        if (uhrzeitID[index].TerminID != null){
+          
+        a[uhrzeitID[index].TerminID -1].Frei = false;
+        }
+    }
+  res.send(a);
+})
+
+serviceRouter.post("/bestellung/addBestellung", function (req, res)
+{
+  
+      var productsDa = new productsDao(req.app.locals.dbConnection);
+      var userID = req.body.bestellung.UserID;
+      var terminID = req.body.bestellung.TerminID;
+      var lieferartID = req.body.bestellung.LieferartID;
+
+      var response = productsDa.addToBestellung(userID, terminID,lieferartID)
+      res.status(200).send("hat geklappt")
+
+});
+
+serviceRouter.get("/bestellung/zahlungsart", function (req,res)
+{
+
+
+  var productsDa = new productsDao(req.app.locals.dbConnection)
+  var zahlungsartAllgemein = productsDa.getZahlungsart();   
+  var zahlungsartUser = productsDa.getZahlungsartUser(req.query.Id);
+  console.log(zahlungsartAllgemein, zahlungsartUser);
+
+  zahlungsartUser.ZahlungsartID = 2;
+
+      var alterWert
+    for (let i = 0; i < zahlungsartAllgemein.length; i++) {
+    const zahlungsart = zahlungsartAllgemein[i];
+    if (zahlungsart.ZahlungsartID==zahlungsartUser.ZahlungsartID) {
+      alterWert = zahlungsartAllgemein[0]
+      zahlungsartAllgemein[0]= zahlungsart  
+      zahlungsartAllgemein[i]= alterWert
+    }
+  }
+  console.log(zahlungsartAllgemein);
+  res.send(zahlungsartAllgemein);
+  res.status(200).end()
 });
 
 
+
+serviceRouter.get("/kassenzettel", function (req, res){            
+
+
+  var gerichteSql = [] 
+  var productsDa = new productsDao(req.app.locals.dbConnection)
+
+  var products = productsDa.getSpeisekorb(req.query.Id);            
+  console.log(products[0].GerichtId);                             
+  products.forEach(element => {
+  gerichteSql.push(productsDa.getGericht(element.GerichtID));  
+    
+  });
+
+  var zeit = productsDa.getZeit(req.query.Id);
+  var kunde = productsDa.getKunde(req.query.Id);
+  var zahlung = productsDa.getZahlungsartKassenzettel(req.query.Id);
+  var bestellart = productsDa.getBestellart(req.query.Id);
+   
+
+  var gesamt = {
+    "Gerichte": gerichteSql,
+    "Zeit": zeit,
+    "Kunde": kunde,
+    "Zahlung": zahlung,
+    "Bestellart": bestellart
+  }
+                   
+  res.status(200).send(gesamt)                                         
+  });
 
 
 module.exports = serviceRouter;
