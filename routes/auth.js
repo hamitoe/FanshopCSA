621ec93ad88aca4779cfb24e8e7d0ad974ac2e1e
CarinaSchulz <schulzc3@hs-albsig.de>
2020-11-29 16:21:24 +0100

    KOMPLETT GELÖSCHT

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const authController = require('../backend/services/auth');
-const router = express.Router();
-
-router.post('/register', authController.register)
-
-module.exports = router;
