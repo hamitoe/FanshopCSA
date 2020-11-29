51f9e18cc03985cdb467d72997cce2a2ee1027cb
hamitoe <72821447+hamitoe@users.noreply.github.com>
2020-11-29 16:15:54 +0100

    #

--- app.js
+++ app.js
@@ -1,83 +1,50 @@
 const express = require('express')
 const app = express()
 const port = 3001
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./backend/db/sortiment.sqlite');
 const router = express.Router();
-var path = require('path');
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+const path = require("path");
+const cookieParser = require('cookie-parser');
 
-// db.serialize(function() {
+const Database = require("better-sqlite3");
+const dbOptions = { verbose: console.log };
 
-//   db.run('CREATE TABLE lorem (info TEXT)');
-//   var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+const dbFile = "./backend/db/sortiment.sqlite";
+const dbConnection = new Database(dbFile, dbOptions);
 
-//   for (var i = 0; i < 10; i++) {
-//     stmt.run('Ipsum ' + i);
-//   }
+app.locals.dbConnection = dbConnection;
+const publicDirectory = path.join(__dirname, './frontend');
+app.use(express.static(publicDirectory));
 
-//   stmt.finalize();
+app.set('views', path.join(__dirname, './frontend'));
+app.set('view engine', 'hbs');
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(function (request, response, next) {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  response.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+// Parse URL-Encoded bodies (as sent by HTML Forms)
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cookieParser());
 
-//   db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
-//     console.log(row.id + ': ' + row.info);
-//   });
-// });
-
-// db.close();
+// Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'))
 
 
-
-app.use(express.static(__dirname + '/frontend'));
-
-app.get('/gsstore/agb', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/agb.html'));
-});
-app.get('/gsstore/bestellbestaedigung', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/Bestellbestätigung.html'));
-});
-app.get('/gsstore/shop/frauen', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/frauen.html'));
-});
-app.get('/gsstore/impressum', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/impressum.html'));
-});
-app.get('/gsstore/kader', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/kader.html'));
-});
-app.get('/gsstore', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/index.html'));
-});
-app.get('/gsstore/shop/kinder', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/kinder.html'));
-});
-app.get('/gsstore/kontakt', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/kontakt.html'));
-});
-app.get('/gsstore/login', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/login.html'));
-});
-app.get('/gsstore/shop/maenner', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/maenner.html'));
-});
-app.get('/gsstore/registration', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/registration.html'));
-});
-app.get('/gsstore/shop', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/shop.html'));
-});
-app.get('/gsstore/shop2', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/shop2.html'));
-});
-app.get('/gsstore/ueberuns', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/ueberuns.html'));
-});
-app.get('/gsstore/warenkorb', function(req, res) {
-  res.sendFile(path.join(__dirname + '/frontend/warenkorb.html'));
-});
+const TOPLEVELPATH = "/gs";
+var serviceRouter = require("./backend/services/products.js");
+app.use(TOPLEVELPATH, serviceRouter);
 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-module.exports =  router;
+module.exports = router;
 
